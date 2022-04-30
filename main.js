f475e4823fec5aa05fa9fd6907ed07956f0bdaea
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2022-04-30 17:11:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(500,450);
@@ -15,5 +19,17 @@ function modeloaded(){
 function gotPose(result){
 if(result.length>0){
     console.log("result")
+
+    leftWristX=result[0].pose.leftWrist.x;
+    rightWristX=result[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
 }
+}
+
+function draw(){
+    background('grey');
+    textSize(difference);
+    text('Thaniksha',50,400);
+    fill('black')
+
 }
